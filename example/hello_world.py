fa5edfc17fb26583b1e15833ffe6ac331c03b951
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2025-06-25 14:39:30 +0200

    feat(example): add hello world example

---
+++ example/hello_world.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+import pandas as pd
+
+def main():
+    print(f'Welcome to ML: \n'
+          f'{torch.__version__}\n'
+          f'numpy:{np.__version__}\n'
+          f'pandas:{pd.__version__}')
+
+if __name__ == "__main__":
+    main()
