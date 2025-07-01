460a93a494bfb50b8b5b9970428bb84f2dacab8a
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2025-07-01 10:08:45 +0200

    chore(example): reformat code

--- example/hello_world.py
+++ example/hello_world.py
@@ -2,11 +2,10 @@ import torch
 import numpy as np
 import pandas as pd
 
+
 def main():
-    print(f'Welcome to ML: \n'
-          f'{torch.__version__}\n'
-          f'numpy:{np.__version__}\n'
-          f'pandas:{pd.__version__}')
+    print(f'Welcome to ML:\n\tpytorch: v{torch.__version__}\n\tnumpy: v{np.__version__}\n\tpandas: v{pd.__version__}')
+
 
 if __name__ == "__main__":
     main()
