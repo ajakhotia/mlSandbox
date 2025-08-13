6eed4e5ff212483ef1d0c997d3238536273e2762
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2025-08-13 16:27:34 +0200

    feat(ci, example): add ci test and rename to echo config

---
+++ example/echo_configuration.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import pandas as pd
+
+
+def main():
+    print(f'pytorch: v{torch.__version__}\nnumpy: v{np.__version__}\npandas: v{pd.__version__}')
+
+
+if __name__ == "__main__":
+    main()
